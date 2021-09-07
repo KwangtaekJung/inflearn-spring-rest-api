307abc2cc00c9945d894bc8c90b49754580ad000
KwangtaekJung <kt79.jung@gmail.com>
2021-09-07 16:14:05 +0900

    Event 생성 API: 매개변수를 이용한 테스트

--- src/test/java/com/example/infleranspringrestapi/event/EventTest.java
+++ src/test/java/com/example/infleranspringrestapi/event/EventTest.java
@@ -1,6 +1,11 @@
 package com.example.infleranspringrestapi.event;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,37 +36,29 @@ class EventTest {
         assertThat(event.getDescription()).isEqualTo(description);
     }
 
-    @Test
-    public void testFree() {
+    @ParameterizedTest
+    @MethodSource("parametersForFreeTest")
+    public void testFree(int basePrice, int maxPrice, boolean isFree) {
         //given
         Event event = Event.builder()
-                .basePrice(0)
-                .maxPrice(0)
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
 
         //when
         event.update();
 
         //then
-        assertThat(event.isFree()).isTrue();
+        assertThat(event.isFree()).isEqualTo(isFree);
+    }
 
-        //when
-        event = Event.builder()
-                .basePrice(100)
-                .maxPrice(0)
-                .build();
-
-        //then
-        assertThat(event.isFree()).isFalse();
-
-        //when
-        event = Event.builder()
-                .basePrice(0)
-                .maxPrice(100)
-                .build();
-
-        //then
-        assertThat(event.isFree()).isFalse();
+    static Stream<Arguments> parametersForFreeTest() {
+        return Stream.of(
+                Arguments.arguments(0, 0, true),
+                Arguments.arguments(100, 0, false),
+                Arguments.arguments(0, 100, false),
+                Arguments.arguments(100, 200, false)
+        );
     }
 
     @Test
