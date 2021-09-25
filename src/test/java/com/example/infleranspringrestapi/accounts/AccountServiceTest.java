8146fc837fd58563e18a5dc5e68685adebb3f5e6
KwangtaekJung <kt79.jung@gmail.com>
2021-09-25 09:36:25 +0900

    스프링 시큐리티 적용

---
+++ src/test/java/com/example/infleranspringrestapi/accounts/AccountServiceTest.java
@@ -0,0 +1,45 @@
+package com.example.infleranspringrestapi.accounts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AccountServiceTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    public void findByUsername() {
+        //given
+        String password = "keesun";
+        String username = "keesun@email.com";
+        Account account = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+        this.accountRepository.save(account);
+
+        //when
+        UserDetailsService userDetailsService = (UserDetailsService) accountService;
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        //then
+        assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+}
